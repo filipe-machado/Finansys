4e9df6a7b2d78611770312006161c5e9344dd678
filipe-machado <ax-filipe@hotmail.com>
2018-12-02 00:18:33 -0500

    Creating routers modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' } //..nomedosite.com/categories -> cai dentro do módulo passado
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//nomedosite.com/categories -> list (Master)
+//nomedosite.com/categories/23 -> form (Detail)
---
+++ src/app/pages/categories/categories-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+
+const routes: Routes = [
+  { path: '', component: CategoryListComponent }, //..deixado em branco para entrar diretamente pela URI, do contrário seria nomedosite.com/categories/caminhoEspecificado
+  { path: 'new', component: CategoryFormComponent }, //..quando for passado o ID, cairá diretamente no form
+  { path: ':id/edit', component: CategoryFormComponent }
+];
+
+//nomedosite.com/categories -> list (Master) carrega a lista
+//nomedosite.com/categories/new -> form (Detail) novo recurso
+//nomedosite.com/categories/:id/edit -> form (Detail) edita o recurso
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CategoriesRoutingModule { }
