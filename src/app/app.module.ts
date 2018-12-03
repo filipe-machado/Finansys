1b3e39ff95ce986495156d08c3fdeaa63000f255
filipe-machado <ax-filipe@hotmail.com>
2018-12-03 11:07:20 -0500

    Intercepting http request with in-memory-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// importação necessária para a aplicação não realizar busca numa api externa
+// deve ser removido em uma api real
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+// deve ser removido em uma api real
+import { InMemoryDatabase } from "./in-memory-database";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) // deve ser removido em uma api real
   ],
   providers: [],
   bootstrap: [AppComponent]
